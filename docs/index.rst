6a103f63977f81952a495fd0ce51f28379480276
Alex Dusenbery <adusenbery@edx.org>
2020-08-24 12:12:02 -0400

    initial commit

---
+++ docs/index.rst
@@ -0,0 +1,29 @@
+.. edx_rbac_demo documentation top level file, created by
+   sphinx-quickstart on Mon Aug 24 12:02:31 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+edx_rbac_demo
+=============
+
+Demo of edx-rbac
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   readme
+   getting_started
+   testing
+   internationalization
+   modules
+   changelog
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ edx_rbac_demo/settings/utils.py
@@ -0,0 +1,89 @@
+import platform
+import sys
+from logging.handlers import SysLogHandler
+from os import environ, path
+
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_setting(setting):
+    """ Get the environment setting or raise exception """
+    try:
+        return environ[setting]
+    except KeyError:
+        error_msg = "Set the [%s] env variable!" % setting
+        raise ImproperlyConfigured(error_msg)
+
+def get_logger_config(log_dir='/var/tmp',
+                      logging_env="no_env",
+                      edx_filename="edx.log",
+                      dev_env=False,
+                      debug=False,
+                      service_variant='edx_rbac_demo'):
+    """
+    Return the appropriate logging config dictionary. You should assign the
+    result of this to the LOGGING var in your settings.
+    """
+
+    hostname = platform.node().split(".")[0]
+    syslog_format = (
+        "[service_variant={service_variant}]"
+        "[%(name)s][env:{logging_env}] %(levelname)s "
+        "[{hostname}  %(process)d] [%(filename)s:%(lineno)d] "
+        "- %(message)s"
+    ).format(
+        service_variant=service_variant,
+        logging_env=logging_env, hostname=hostname
+    )
+
+    handlers = ['console']
+
+    logger_config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s %(levelname)s %(process)d '
+                          '[%(name)s] %(filename)s:%(lineno)d - %(message)s',
+            },
+            'syslog_format': {'format': syslog_format},
+            'raw': {'format': '%(message)s'},
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG' if debug else 'INFO',
+                'class': 'logging.StreamHandler',
+                'formatter': 'standard',
+                'stream': sys.stdout,
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers': handlers,
+                'propagate': True,
+                'level': 'INFO'
+            },
+            'requests': {
+                'handlers': handlers,
+                'propagate': True,
+                'level': 'WARNING'
+            },
+            'factory': {
+                'handlers': handlers,
+                'propagate': True,
+                'level': 'WARNING'
+            },
+            'django.request': {
+                'handlers': handlers,
+                'propagate': True,
+                'level': 'WARNING'
+            },
+            '': {
+                'handlers': handlers,
+                'level': 'DEBUG',
+                'propagate': False
+            },
+        }
+    }
+
+    return logger_config
