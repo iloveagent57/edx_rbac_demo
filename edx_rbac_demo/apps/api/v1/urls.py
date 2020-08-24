25b206f191da05911e4525bee34ca9730549e3ac
Alex Dusenbery <adusenbery@edx.org>
2020-08-24 16:37:48 -0400

    get requests working for an authenticated superuser

--- edx_rbac_demo/apps/api/v1/urls.py
+++ edx_rbac_demo/apps/api/v1/urls.py
@@ -1,4 +1,14 @@
 """ API v1 URLs. """
+from django.conf.urls import url
+from rest_framework.routers import DefaultRouter
+
+from edx_rbac_demo.apps.api.v1 import views
+
 
 app_name = 'v1'
 urlpatterns = []
+
+router = DefaultRouter()  # pylint: disable=invalid-name
+router.register(r'users', views.UserViewSet, basename='users')
+
+urlpatterns += router.urls
--- edx_rbac_demo/apps/api/v1/views.py
+++ edx_rbac_demo/apps/api/v1/views.py
@@ -1,1 +1,45 @@
 # Create your views here.
+from edx_rbac.mixins import PermissionRequiredMixin
+from edx_rest_framework_extensions.auth.jwt.authentication import (
+    JwtAuthentication,
+)
+from rest_framework import permissions, viewsets, serializers
+
+from edx_rbac_demo.apps.core.models import User
+
+
+class BaseViewSet(PermissionRequiredMixin, viewsets.ViewSet):
+    """
+    Base class for all view sets.
+    """
+    # Adding this means you'll have to have valid edX JWT cookies
+    # loaded in your browser (if you make requests via the browser).
+    # You have to make requests with the 'use-jwt-cookie' header to make this so.
+    authentication_classes = [JwtAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id', 'email', 'first_name', 'last_name', 'is_superuser', 'is_staff'
+        ]
+
+
+class UserViewSet(BaseViewSet, viewsets.ModelViewSet):
+    """
+    View set for getting/setting data about users.
+    """
+    queryset = User.objects.all().order_by('email')
+    permission_required = 'demo.has_learner_access'
+    serializer_class = UserSerializer
+
+    def get_permission_object(self):
+        """
+        Retrieves the apporpriate object to use during edx-rbac's permission checks.
+
+        This object is passed to the rule predicate(s).
+        """
+        #import pdb; pdb.set_trace()
+        return self.get_object()
